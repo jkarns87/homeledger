bdb521308ac696add5715c14049035dfbaf0c283
Joseph Karns <jkarns87@gmail.com>
2026-09-15 10:21:55 -0400

    feat(core): sample service-provider marketplace

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ export const CORE_VERSION = '0.1.0';
 export * from './ids.js';
 export * from './domain/schemas.js';
 export * from './domain/maintenance.js';
+export * from './marketplace/providers.js';
 export * from './repo/keys.js';
 export type { Repository } from './repo/repository.js';
 export { createMemoryRepository } from './repo/memory.js';
